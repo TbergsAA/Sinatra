d8f91834a8f66eb49a0fd077303cac9dcb623995
TbergsAA <at0892@inbox.lv>
2015-03-08 23:09:20 +0200

    upgrade.

--- basics.rb
+++ basics.rb
@@ -1,29 +1,74 @@
 require 'sinatra'
+require 'pry'
+  USERS = []
 
-# get '/more/*' do
-#   params[:splat]
-# end
+  get '/' do  
+    erb :form
+  end
 
-get '/' do
-  erb :form
+  post '/' do
+    log_in = UserLogIn.new(params)
+    if log_in.authorization
+     erb :success_log_in
+    else
+    redirect to('/?message=incorect_username_or_password')
+    end
+  end
+
+  get '/register' do
+    erb :register
+  end
+
+  post '/register' do
+    user = User.new(params)
+    response = user.save
+    if response == "true"
+      redirect to ('/?message=REGISTER_SUCCESS')
+    else
+      redirect to ("/register?message=#{response}")
+    end
+  end
+
+class User
+
+  attr_reader :first_name, :last_name, :username, :email, :password, :bday, :password_con
+  
+  def initialize(params)
+    @first_name = params.fetch("fname", nil)
+    @last_name = params.fetch("lname", nil)
+    @username = params.fetch("uname", nil)
+    @email = params.fetch("email", nil)
+    @password = params.fetch("password", nil)
+    @password_con = params.fetch("password_con", nil)
+    @bday = params.fetch("bday", nil)
+  end
+
+  def save
+    if password == password_con
+      USERS << self
+      "true"
+    else
+      "Wrong_password"
+    end
+  end
 end
 
-post '/' do
-  "Hi, you are loged in!"
+class UserLogIn
+  attr_reader :username, :password
+
+  def initialize(params)
+    @username = params.fetch("username", nil)
+    @password = params.fetch("password", nil)
+  end
+
+  def authorization
+    valid = false
+    USERS.each do |n|
+      if n.username == username && n.password == password
+        valid = true
+        break
+      end
+    end
+    valid
+  end
 end
-
-# get '/secret' do
-#   erb :secret
-# end
-
-# post '/secret' do
-#   params[:secret].reverse
-# end
-
-# get '/decrypt/:secret' do
-#   params[:secret].reverse
-# end
-
-# not_found do
-#   halt 404, 'page not found'
-# end
